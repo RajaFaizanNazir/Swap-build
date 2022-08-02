3e40f46766445813f49651c797d769f82c771a29
Raja Faizan Nazir <72030804+RajaFaizanNazir@users.noreply.github.com>
2022-08-02 17:35:46 +0500

    Add files via upload

---
+++ precache-manifest.648af35628fac0e1ca038d49bb9fbc34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb9d628300e8949d4f446b690b96f7ea",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cdfb95ff4ff5d6d9e637",
+    "url": "./static/css/2.3870cfe2.chunk.css"
+  },
+  {
+    "revision": "9d182bc25f5855d1a46d",
+    "url": "./static/css/main.fbe92ca0.chunk.css"
+  },
+  {
+    "revision": "cdfb95ff4ff5d6d9e637",
+    "url": "./static/js/2.11d66e4a.chunk.js"
+  },
+  {
+    "revision": "67238a5571fedc136fefc667d67332b0",
+    "url": "./static/js/2.11d66e4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d182bc25f5855d1a46d",
+    "url": "./static/js/main.dfce812d.chunk.js"
+  },
+  {
+    "revision": "d852897cea0a694b3726",
+    "url": "./static/js/runtime-main.c06bb91b.js"
+  }
+]);
